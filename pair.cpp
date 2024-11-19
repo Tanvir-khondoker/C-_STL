faed983996be5b142bcc83662bab971caef6a8e1
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2024-11-19 10:52:36 +0600

    file name changed

---
+++ pair.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+
+    pair<int, string> p;
+    //p = make_pair(3, "abc");
+    p = {2, "abcd"};
+
+    cout<<p.first;
+
+    return 0;
+}
